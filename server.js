ac23226f887d31a40e0c12a035d37adfbcdfb1da
Aleem Ahmad <ahmadaleem592@gmail.com>
2025-11-27 17:14:54 +0500

    updated files

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "http://localhost:3001",
+  "https://womanarafoundation.org",
   "https://womanarafoundation.org/"
 ];
 
